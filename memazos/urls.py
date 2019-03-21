dcd0ebeeaa7dcd4d247ec8e7e4f8a6544a868098
Magdiel Juarez Guerrero <mjua.guerrero@gmail.com>
2019-03-21 09:12:07 -0600

    new views and minor changes

--- memazos/urls.py
+++ memazos/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('patrones_primero', views.patrones1, name='patrones1'),
+    path('patrones_segundo', views.patrones2, name='patrones2'),
     # path('/elementos_basic', views.elementos, name='elementos'),
 ]
--- memazos/views.py
+++ memazos/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+def patrones1(request):
+    return render(request, 'patrones_1.html', {})
+
+def patrones2(request):
+    return render(request, 'patrones_2.html', {})
